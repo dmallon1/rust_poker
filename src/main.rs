c4758ccdc2de6d69fe3a4543e7357d94ee23b038
Dan Mallon <d.mallon@me.com>
2023-01-07 15:29:06 -0500

    remove warnings in main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,17 @@ fn main() {
     // number of players?
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("problem getting args: {}", err);
+    let config = Config::new(&args).unwrap_or_else(|_| {
+        eprintln!("problem getting args, expected something like \"cargo run -- 4\"");
         process::exit(1);
     });
 
     println!("You've selected {} players.", config.number_of_players);
 
     // start game
-    rust_poker::play_game(config.number_of_players);
+    let result = rust_poker::play_game(config.number_of_players);
+    match result {
+        Ok(_) => println!("thanks for playing"),
+        Err(msg) => println!("{}", msg),
+    }
 }
